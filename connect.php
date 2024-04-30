7e7bbbc2be23331aad2a28e580e7a883d9ffc4db
rajashwis <rajashwis@gmail.com>
2024-04-30 21:13:09 +0545

    added connect.php and sign out.php files

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$connection = oci_connect("cfx_12", "cfxadmin#22", "//localhost/xe");
+
+if (!$connection) {
+    $error_message = oci_error();
+    echo "Failed to connect to Oracle: " . $error_message['message'];
+    exit();
+}
+
+?>
