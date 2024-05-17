0064f3797c32623f91b022921f6fe839ce445b95
rajashwis <rajashwis@gmail.com>
2024-05-17 08:55:55 +0545

    Fixed homepage and backend sql

--- backend/sequences.sql
+++ backend/sequences.sql
@@ -1,3 +1,13 @@
+DROP SEQUENCE seq_cart_id;
+DROP SEQUENCE seq_customer_id;
+DROP SEQUENCE seq_product_id;
+DROP SEQUENCE seq_wishlistproduct_id;
+DROP SEQUENCE seq_cartproduct_id;
+DROP SEQUENCE seq_orderdetail_id;
+DROP SEQUENCE seq_orderproduct_id;
+DROP SEQUENCE seq_shop_id;
+DROP SEQUENCE seq_category_id;
+
 CREATE SEQUENCE seq_cart_id
     MINVALUE 1
     MAXVALUE 999
@@ -8,8 +18,64 @@ CREATE SEQUENCE seq_cart_id
     
 CREATE SEQUENCE seq_customer_id
     MINVALUE 0001
+    MAXVALUE 9999
+    INCREMENT BY 1
+    START WITH 0001
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_product_id
+    MINVALUE 00001
     MAXVALUE 99999
     INCREMENT BY 1
     START WITH 0001
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_wishlistproduct_id
+    MINVALUE 111
+    MAXVALUE 9999
+    INCREMENT BY 1
+    START WITH 111
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_cartproduct_id
+    MINVALUE 200
+    MAXVALUE 9999
+    INCREMENT BY 1
+    START WITH 200
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_orderdetail_id
+    MINVALUE 11111
+    MAXVALUE 99999
+    INCREMENT BY 1
+    START WITH 11111
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_orderproduct_id
+    MINVALUE 1
+    MAXVALUE 9999
+    INCREMENT BY 1
+    START WITH 1
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_shop_id
+    MINVALUE 2222
+    MAXVALUE 5555
+    INCREMENT BY 1
+    START WITH 2222
+    NOCACHE
+    NOCYCLE;
+    
+CREATE SEQUENCE seq_category_id
+    MINVALUE 444
+    MAXVALUE 5555
+    INCREMENT BY 1
+    START WITH 444
     NOCACHE
     NOCYCLE;
--- homepage/home.php
+++ homepage/home.php
@@ -251,65 +251,55 @@ $user = $_SESSION['user_id'];
     <div class="for-you">
         <label class="text-foryou">For You:</label>
         <div class="container-5">
-            <div class="card-6">
-                <div class="image-container">
-                    <img src="../resources/products/jordan.jpg" alt="jordan">
-                </div>
-                <h1>AIR JORDAN 1 <span class="purple-text">Purple</span></h1>
-                <p class="price"><s>$64.99</s><br>$49.99 <i class="fa-solid fa-tag"></i></p>
-                <p>Men's Shoes</p>
-                <p><button>Buy</button></p><br>
-            </div>
-        
-            <div class="card-6">
-                <div class="image-container">
-                    <img src="../resources/products/watch.jpg" alt="jordan">
-                </div>
-                <h1>AIR JORDAN 1 <span class="purple-text">Purple</span></h1>
-                <p class="price"><s>$64.99</s><br>$49.99 <i class="fa-solid fa-tag"></i></p>
-                <p>Men's Shoes</p>
-                <p><button>Buy</button></p><br>
-            </div>
-        
-            <div class="card-6">
-                <div class="image-container">
-                    <img src="../resources/products/airpod.jpg" alt="jordan">
-                </div>
-                <h1>AIR JORDAN 1 <span class="purple-text">Purple</span></h1>
-                <p class="price"><s>$64.99</s><br>$49.99 <i class="fa-solid fa-tag"></i></p>
-                <p>Men's Shoes</p>
-                <p><button>Buy</button></p><br>
-            </div>
 
-            <div class="card-6">
-                <div class="image-container">
-                    <img src="../resources/products/jordan.jpg" alt="jordan">
-                </div>
-                <h1>AIR JORDAN 1 <span class="purple-text">Purple</span></h1>
-                <p class="price"><s>$64.99</s><br>$49.99 <i class="fa-solid fa-tag"></i></p>
-                <p>Men's Shoes</p>
-                <p><button>Buy</button></p><br>
-            </div>
-        
-            <div class="card-6">
-                <div class="image-container">
-                    <img src="../resources/products/watch.jpg" alt="jordan">
-                </div>
-                <h1>AIR JORDAN 1 <span class="purple-text">Purple</span></h1>
-                <p class="price"><s>$64.99</s><br>$49.99 <i class="fa-solid fa-tag"></i></p>
-                <p>Men's Shoes</p>
-                <p><button>Buy</button></p><br>
-            </div>
-        
-            <div class="card-6">
-                <div class="image-container">
-                    <img src="../resources/products/airpod.jpg" alt="jordan">
-                </div>
-                <h1>AIR JORDAN 1 <span class="purple-text">Purple</span></h1>
-                <p class="price"><s>$64.99</s><br>$49.99 <i class="fa-solid fa-tag"></i></p>
-                <p>Men's Shoes</p>
-                <p><button>Buy</button></p><br>
-            </div>
+            <?php
+                $query = "SELECT *
+                FROM (
+                    SELECT *
+                    FROM PRODUCT
+                    ORDER BY DBMS_RANDOM.VALUE
+                )
+                WHERE ROWNUM <= 6";
+                $stid=oci_parse($connection, $query);
+                oci_execute($stid);
+
+                while(($product = oci_fetch_assoc($stid))) {
+                    $product_id = $product['PRODUCT_ID'];
+                    
+                    $imageData = $product['PRODUCT_IMAGE']->load();
+                    $encodedImageData = base64_encode($imageData);
+                    // Determine the image type based on the first few bytes of the image data
+                    $header = substr($imageData, 0, 4);
+                    $imageType = 'image/jpeg'; // default to JPEG
+                    if (strpos($header, 'FFD8') === 0) {
+                        $imageType = 'image/jpeg'; // JPEG
+                    } elseif (strpos($header, '89504E47') === 0) {
+                        $imageType = 'image/png'; // PNG
+                    }
+
+                    $category_id = $product['CATEGORY_ID'];
+                    $query1 = "SELECT CATEGORY_NAME FROM PRODUCTCATEGORY WHERE CATEGORY_ID = $category_id";
+                    $stid1=oci_parse($connection, $query1);
+                    oci_execute($stid1);
+                    $category = oci_fetch_assoc($stid1);
+                    $category = $category['CATEGORY_NAME'];
+
+                    echo '<div class="card-6">';
+                    echo '<div class="image-container">';
+                    echo '<a href = "../product_detail/product_detail.php?product_id='.$product_id.'">';
+                    echo '<img src="data:' . $imageType . ';base64,' . $encodedImageData . '" alt="Uploaded Image">';
+                    echo '</div>';
+                    echo '<h1>'.$product['PRODUCT_NAME'].'</h1>';
+                    echo '</a>';
+                    echo '<p class="price"><br>'.$product['PRICE'].'&nbsp<i class="fa-solid fa-tag"></i></p>';
+                    echo '<a href = "../category/categories.php?category='.$category.'">';
+                    echo '<p>'.$category.'</p>';
+                    echo '</a>';
+                    echo '<p><button>Buy</button></p><br>';
+                    echo '</div>';
+                }
+
+            ?>
         </div>
             <br><br><br><br><br><br><br><br><br><br><br>
             <!--map-->
